20b5ed0270487317f23f150a372068a3f61f836e
markosantonio19 <1433811@senati.pe>
2023-08-31 16:57:01 -0500

    aumentar el valor

--- calculadora/Form1.cs
+++ calculadora/Form1.cs
@@ -10,61 +10,61 @@ namespace calculadora
         private void button8_Click(object sender, EventArgs e)
         {
             //boton 6
-            tbDisplay.Text = "6";
+            tbDisplay.Text = tbDisplay.Text + "6";
         }
 
         private void btn0_Click(object sender, EventArgs e)
         {
             //boton 0
-            tbDisplay.Text = "0";
+            tbDisplay.Text = tbDisplay.Text + "0";
         }
 
         private void btn1_Click(object sender, EventArgs e)
         {
             //boton 1
-            tbDisplay.Text = "1";
+            tbDisplay.Text = tbDisplay.Text + "1";
         }
 
         private void btn2_Click(object sender, EventArgs e)
         {
             //boton 2
-            tbDisplay.Text = "2";
+            tbDisplay.Text = tbDisplay.Text + "2";
         }
 
         private void btn3_Click(object sender, EventArgs e)
         {
             //boton 3
-            tbDisplay.Text = "3";
+            tbDisplay.Text = tbDisplay.Text + "3";
         }
 
         private void btn4_Click(object sender, EventArgs e)
         {
             //boton 4
-            tbDisplay.Text = "4";
+            tbDisplay.Text = tbDisplay.Text + "4";
         }
 
         private void btn5_Click(object sender, EventArgs e)
         {
             //boton 5
-            tbDisplay.Text = "5";
+            tbDisplay.Text = tbDisplay.Text + "5";
         }
 
         private void btn7_Click(object sender, EventArgs e)
         {
             //boton 7
-            tbDisplay.Text = "7";
+            tbDisplay.Text = tbDisplay.Text + "7";
         }
 
         private void btn8_Click(object sender, EventArgs e)
         {
             //boton 8
-            tbDisplay.Text = "8";
+            tbDisplay.Text = tbDisplay.Text + "8";
         }
 
         private void btn9_Click(object sender, EventArgs e)
         {
             //boton 9
-            tbDisplay.Text = "9";
+            tbDisplay.Text = tbDisplay.Text + "9";
         }
     }
 }
