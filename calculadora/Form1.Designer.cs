1c42cd50892ada07acb2db5b273ccbfe638d9c84
markosantonio19 <1433811@senati.pe>
2023-08-31 16:23:46 -0500

    interfaz calculadora

--- calculadora/Form1.Designer.cs
+++ calculadora/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(371, 450);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
--- calculadora/Form1.cs
+++ calculadora/Form1.cs
@@ -7,6 +7,8 @@ namespace calculadora
             InitializeComponent();
         }
 
-       
+
+
+
     }
 }
